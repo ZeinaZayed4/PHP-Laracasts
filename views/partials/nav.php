281abf1b812ba09fcb074b0c18f77aa5b18e7473
ZeinaZayed4 <zeinazayed@std.mans.edu.eg>
2024-12-15 17:06:47 +0200

    Make a PHP router

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -10,8 +10,8 @@
 					<div class="ml-10 flex items-baseline space-x-4">
 						<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                         <a href="/" class="<?= urlIs('/') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
-						<a href="/about.php" class="<?= urlIs('/about.php') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">About</a>
-						<a href="/contact.php" class="<?= urlIs('/contact.php') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Contact</a>
+						<a href="/about" class="<?= urlIs('/about') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">About</a>
+						<a href="/contact" class="<?= urlIs('/contact') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Contact</a>
 					</div>
 				</div>
 			</div>
