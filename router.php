039da547709a6a5f23b66bab1932a66bdaa71de0
ZeinaZayed4 <zeinazayed@std.mans.edu.eg>
2024-12-26 19:35:59 +0200

    Intro to forms and request methods

--- router.php
+++ router.php
@@ -1,14 +1,6 @@
 <?php
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-	'/' => 'controllers/index.php',
-	'/about' => 'controllers/about.php',
-	'/notes' => 'controllers/notes.php',
-	'/note' => 'controllers/note.php',
-	'/contact' => 'controllers/contact.php',
-];
+$routes = require('routes.php');
 
 function routeToController($uri, $routes)
 {
@@ -25,5 +17,7 @@ function abort($code = 404)
 	require "views/{$code}.php";
 	die();
 }
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 routeToController($uri, $routes);
