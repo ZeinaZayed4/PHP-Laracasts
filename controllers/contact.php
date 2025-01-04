f656af3e1a981aae1ea52fd5340914d1e013f696
ZeinaZayed4 <zeinazayed@std.mans.edu.eg>
2025-01-04 20:54:03 +0200

    Sessions

--- controllers/contact.php
+++ controllers/contact.php
@@ -1,5 +1,7 @@
 <?php
 
+$_SESSION['last'] = 'Zayed';
+
 view('contact.view.php', [
 	'heading' => 'Contact Us'
 ]);
--- controllers/index.php
+++ controllers/index.php
@@ -1,5 +1,7 @@
 <?php
 
+$_SESSION['name'] = 'Zeina';
+
 view('index.view.php', [
 	'heading' => 'Home'
 ]);
